b46a45178d290caf60028255db78edd5efa509ca
MuhammedAnasEP <anasep1110@gmail.com>
2023-09-08 09:37:57 +0530

    added multitable model inheritance and proxy model inheritance

--- MyProject/MyApp/admin.py
+++ MyProject/MyApp/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import ContactInfo, Customer, Staff
+from .models import ContactInfo, Customer, Staff, Place, Restaurant, Person, MyPerson
 
 # Register your models here.
 
 # admin.site.register(ContactInfo) --> it will show an error becouse its abstract class
 admin.site.register(Customer)
 admin.site.register(Staff)
+admin.site.register(Place)
+admin.site.register(Restaurant)
+admin.site.register(Person)
+admin.site.register(MyPerson)
--- MyProject/MyApp/models.py
+++ MyProject/MyApp/models.py
@@ -19,3 +19,42 @@ class Staff(ContactInfo):
     position = models.CharField(max_length=30)
 
 # ------------------- End of Abstract class model inheritace ---------------------------
+
+
+# Multi Table model inheritance
+
+class Place(models.Model):
+    name = models.CharField(max_length=30)
+    address = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+class Restaurant(Place):
+    serves_soup = models.BooleanField(default=False)
+    serves_pizza = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.serves_soup
+    
+# ----------------- End of Multitable model inheritance --------------------------------
+
+
+#Proxy model inheritance
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.first_name
+    
+class MyPerson(Person):
+    class Meta:
+        proxy = True
+        # ordering = ["first_name"]
+
+    def __str__(self):
+        return self.first_name
+    
+# ------------------ End of Abstract model inheritance --------------------------------
